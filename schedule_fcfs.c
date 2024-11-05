d8480e3ba5650cec2385e3a84a425cfd759fabf0
ea269 <116852460+ea269@users.noreply.github.com>
2024-11-04 20:56:39 -0800

    Working on fcfs

--- schedule_fcfs.c
+++ schedule_fcfs.c
@@ -9,4 +9,19 @@
 #include "list.h"
 #include "cpu.h"
 
-//Your code and design here
+void schedule() {
+    // Pointer holds current task node in list
+    struct node *current = head;
+
+    // Loop through task list
+    while (current != NULL) {
+        // Get task from current node
+        Task *task = current -> task; 
+
+        // Run task with run function from CPU.c
+        run(task, task -> burst);
+
+        // Cycles next task
+        current = current -> next;
+    }
+}
